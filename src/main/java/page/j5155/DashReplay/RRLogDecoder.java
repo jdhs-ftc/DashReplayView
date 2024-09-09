06e5722cd8b3b68b256fc368fd8c70f795a5327c
j5155 <54331556+j5155@users.noreply.github.com>
2024-09-08 16:26:39 -0800

    move to rr logs, old project was bad

---
+++ src/main/java/page/j5155/DashReplay/RRLogDecoder.java
@@ -0,0 +1,176 @@
+package page.j5155.DashReplay;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class RRLogDecoder {
+    FileInputStream f;
+    int numberOfBytesRead = 0;
+
+    interface MessageSchema {}
+    class StructSchema implements MessageSchema {
+        Map<String,MessageSchema> fields;
+        StructSchema(Map<String,MessageSchema> fields) {
+            this.fields = fields;
+        }
+    }
+    enum PrimitiveSchema implements MessageSchema {
+        INT,
+        LONG,
+        DOUBLE,
+        STRING,
+        BOOLEAN
+    }
+
+    class EnumSchema implements MessageSchema {
+        List<String> constants;
+
+        public EnumSchema(List<String> constants) {
+            this.constants = constants;
+        }
+    }
+
+
+    byte[] read(int n) throws IOException {
+        numberOfBytesRead += n;
+        assert n > 0;
+        // assume this reads exactly n bytes or we reach EOF
+        byte[] buf = new byte[n];
+        int readReturn = f.read(buf, 0, n); // Does this actually buffer? Or does this need a different offset every time? TODO: find out
+        if (readReturn == -1) {
+            throw new EOFException();
+        }
+
+        return buf;
+    }
+    String read_string() throws IOException {
+        int nbytes = ByteBuffer.wrap((read(4))).getInt();
+        return new String(read(nbytes), StandardCharsets.UTF_8);
+    }
+
+    MessageSchema read_schema() throws IOException {
+        int schema_type = ByteBuffer.wrap(read(4)).getInt();
+        if (schema_type == 0) {
+            int nfields = ByteBuffer.wrap(read(4)).getInt();
+            Map<String,MessageSchema> fields = new LinkedHashMap<>();
+            // TODO
+            for (int i = 0; i < nfields; i++) {
+                String name = read_string();
+                fields.put(name, read_schema());
+            }
+            return new StructSchema(fields);
+        // primitive schema
+        } else if (schema_type == 1) {
+            return PrimitiveSchema.INT;
+        } else if (schema_type == 2) {
+            return PrimitiveSchema.LONG;
+        } else if (schema_type == 3) {
+            return PrimitiveSchema.DOUBLE;
+        } else if (schema_type == 4) {
+            return PrimitiveSchema.STRING;
+        } else if (schema_type == 5) {
+            return PrimitiveSchema.BOOLEAN;
+        // enum schema
+        } else if (schema_type == 6) {
+            int nconstants = ByteBuffer.wrap(read(4)).getInt();
+            List<String> constants = new ArrayList<>();
+            for (int i = 0; i < nconstants; i++) {
+                constants.add(read_string());
+            }
+            return new EnumSchema(constants);
+        } else {
+            throw new RuntimeException("Unknown schema type: " + schema_type);
+        }
+
+    }
+    Object read_msg(MessageSchema schema) throws IOException {
+        if (schema instanceof StructSchema) {
+            Dictionary<String,Object> msg = new Hashtable<>();
+            // ending for the night, this corresponds to line 75 of the python code
+            for (String name : ((StructSchema) schema).fields.keySet()) {
+                msg.put(name, read_msg(((StructSchema) schema).fields.get(name))); // this casting is weird
+            }
+            return msg;
+        } else if (schema instanceof PrimitiveSchema) {
+            if (schema == PrimitiveSchema.INT) {
+                return ByteBuffer.wrap(read(4)).getInt();
+            } else if (schema == PrimitiveSchema.LONG) {
+                return ByteBuffer.wrap(read(8)).getLong();
+            } else if (schema == PrimitiveSchema.DOUBLE) {
+                return ByteBuffer.wrap(read(8)).getDouble();
+            } else if (schema == PrimitiveSchema.STRING) {
+                return read_string();
+            } else if (schema == PrimitiveSchema.BOOLEAN) {
+                return read(1)[0] == 1; // todo: this is not gonna work at all
+            } else {
+                throw new RuntimeException("Unknown primitive schema: " + schema);
+            }
+        } else if (schema instanceof EnumSchema) {
+            int ordinal = ByteBuffer.wrap(read(4)).getInt();
+            return ((EnumSchema) schema).constants.get(ordinal);
+        } else {
+            throw new RuntimeException("Unknown schema: " + schema);
+        }
+    }
+
+    List<Map<String, ?>> read_file(File f) throws IOException {
+        this.f = new FileInputStream(f);
+
+        String magic = new String(read(2),StandardCharsets.UTF_8);
+        assert magic.equals("RR") : "This is not a Roadrunner 1.0 log file"; // error added by me j5155
+        short version = ByteBuffer.wrap(read(2)).getShort();
+        assert version == 0 : "File version newer then expected (expected version 0 got version " + version + ")";
+
+        ArrayList<String> channels = new ArrayList<>();
+        Map<String,MessageSchema> schemas = new LinkedHashMap<>();
+        Map<String,ArrayList<Object>> messages = new LinkedHashMap<>(); // not sure whether these types are right
+
+        while (true) {
+            try {
+                int entry_type = ByteBuffer.wrap(read(4)).getInt();
+                if (entry_type == 0) {
+                    // channel definition
+                    String ch = read_string();
+                    schemas.put(ch, read_schema());
+                    channels.add(ch);
+                } else if (entry_type == 1) {
+                    // message
+                    int ch_index = ByteBuffer.wrap(read(4)).getInt();
+                    String ch = channels.get(ch_index);
+                    if (messages.get(ch) == null) {
+                        messages.put(ch, new ArrayList<>());
+                    }
+                    messages.get(ch).add(read_msg(schemas.get(ch)));
+                } else {
+                    throw new RuntimeException("Unknown entry type: " + entry_type);
+                }
+            } catch (EOFException e) {
+                break;
+            }
+        }
+        return Arrays.asList(schemas,messages);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        RRLogDecoder d = new RRLogDecoder();
+        File file = new File("/home/james/Documents/robotlogs/2024_02_16__19_52_54_834__FarParkLeftPixel.log");
+        List<Map<String,?>> fileContents = d.read_file(file);
+        Map<String,MessageSchema> schemas = (Map<String, MessageSchema>) fileContents.get(0);
+        Map<String,ArrayList<Object>> messages = (Map<String, ArrayList<Object>>) fileContents.get(1);
+
+        for (String ch : schemas.keySet()) {
+            MessageSchema schema = schemas.get(ch);
+            System.out.println("Channel: " + ch + " (" + messages.get(ch).size() + "messages)\n " + schema);
+            for (Object o : messages.get(ch)) {
+                System.out.print(o.getClass());
+                System.out.println(o);
+            }
+
+        }
+    }
+
+}
---
+++ src/main/java/page/j5155/DashReplay/ShowLogOnDashOpMode.java
@@ -0,0 +1,163 @@
+package page.j5155.DashReplay;
+
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.config.ValueProvider;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import page.j5155.DashReplay.testopmode.TestOpMode;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class ShowLogOnDashOpMode extends TestOpMode {
+    TestDashboardInstance dashboard;
+    ArrayList<Pose2d> targetPoses = new ArrayList<>();
+    ArrayList<Pose2d> estPoses = new ArrayList<>();
+    ArrayList<Long> targetTimestamps = new ArrayList<>();
+    ArrayList<Long> estTimestamps = new ArrayList<>();
+    long replayStartTime = System.nanoTime();
+    long recordStartTime;
+
+    public ShowLogOnDashOpMode() {
+        super("ShowLogOnDashOpMode");
+    }
+
+    @Override
+    protected void init() {
+        dashboard = TestDashboardInstance.getInstance();
+
+        RRLogDecoder d = new RRLogDecoder();
+        File file = new File(System.getProperty("user.home") + "/Downloads/2024_09_08__12_54_05_427__ManualFeedforwardTuner (1).log");
+        List<Map<String,?>> fileContents = null;
+        try {
+            fileContents = d.read_file(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Map<String, RRLogDecoder.MessageSchema> schemas = (Map<String, RRLogDecoder.MessageSchema>) fileContents.get(0);
+        Map<String, ArrayList<Object>> messages = (Map<String, ArrayList<Object>>) fileContents.get(1);
+
+        for (String ch : schemas.keySet()) {
+            RRLogDecoder.MessageSchema schema = schemas.get(ch);
+            System.out.println("Channel: " + ch + " (" + messages.get(ch).size() + "messages)\n " + schema);
+            if (Objects.equals(ch, "ESTIMATED_POSE")) {
+                for (Object msg: messages.get(ch)) {
+                    Hashtable<String,Number> poseWithTime = (Hashtable<String,Number>) msg;
+                    estPoses.add(new Pose2d((Double) poseWithTime.get("x"), // this casting is weird
+                            (Double) poseWithTime.get("y"),
+                            (Double) poseWithTime.get("heading")));
+                    estTimestamps.add((Long) poseWithTime.get("timestamp"));
+                }
+            } else if (Objects.equals(ch, "TARGET_POSE")) {
+                for (Object msg: messages.get(ch)) {
+                    Hashtable<String,Number> poseWithTime = (Hashtable<String,Number>) msg;
+                    targetPoses.add(new Pose2d((Double) poseWithTime.get("x"),
+                            (Double) poseWithTime.get("y"),
+                            (Double) poseWithTime.get("heading")));
+                    targetTimestamps.add((Long) poseWithTime.get("timestamp"));
+                }
+            }
+            if (messages.get(ch).size() < 3) {
+                for (Object msg: messages.get(ch)) {
+                    System.out.println(msg);
+                }
+            }
+
+        }
+
+
+        if (!estTimestamps.isEmpty()) {
+            recordStartTime = estTimestamps.get(0);
+        }
+    }
+
+    @Override
+    protected void loop() throws InterruptedException {
+        //draw the field
+        TelemetryPacket packet = new TelemetryPacket(false);
+        Canvas c = packet.fieldOverlay();
+        c.setAlpha(0.4)
+            .drawImage("https://raw.githubusercontent.com/acmerobotics/ftc-dashboard/master/FtcDashboard/dash/public/centerstage.webp", 0, 0, 144, 144)
+                .setAlpha(1.0)
+        .drawGrid(0, 0, 144, 144, 7, 7);
+
+        // draw a line of all the target and estimated poses
+        if (!targetPoses.isEmpty()) {
+            c.setStroke("#4CAF50");
+            c.setStrokeWidth(1);
+            drawPoseList(c, targetPoses);
+        }
+        if (!estPoses.isEmpty()) {
+            c.setStroke("#3F51B5");
+            c.setStrokeWidth(1);
+            drawPoseList(c, estPoses);
+        }
+
+        // find the current time, calculate the time since the replay started, and find where we are in the poses
+        // then find which poses to show
+        long offset = System.nanoTime() - replayStartTime;
+
+        if (recordStartTime + offset > estTimestamps.get(estTimestamps.size() - 1)) {
+            // we're past the end of the recorded timestamps, so reset
+            c.fillText("Replay over, looping...",30,50,"Arial",0);
+            dashboard.sendTelemetryPacket(packet);
+            Thread.sleep(1000);
+
+            replayStartTime = System.nanoTime();
+            offset = System.nanoTime() - replayStartTime;
+        }
+        long timeInReplay = recordStartTime + offset;
+
+
+        long estTimestampToShow = estTimestamps.stream().min(Comparator.comparingLong(f -> Math.abs(f - timeInReplay))).orElse(estTimestamps.get(0)); // https://stackoverflow.com/questions/62559012/find-closest-object-in-java-collection-for-a-given-value-using-java8-stream
+        Pose2d estPoseToShow = estPoses.get(estTimestamps.indexOf(estTimestampToShow)); // this seems inefficient
+
+        c.setStroke("#3F51B5");
+        drawRobot(c,estPoseToShow);
+
+        if (!targetTimestamps.isEmpty()) {
+            long targetTimestampToShow = targetTimestamps.stream().min(Comparator.comparingLong(f -> Math.abs(f - timeInReplay))).orElse(targetTimestamps.get(0)); // https://stackoverflow.com/questions/62559012/find-closest-object-in-java-collection-for-a-given-value-using-java8-stream
+            Pose2d targetPoseToShow = targetPoses.get(targetTimestamps.indexOf(targetTimestampToShow)); // this seems inefficient
+
+
+            c.setStroke("#4CAF50");
+            drawRobot(c, targetPoseToShow);
+        }
+
+
+        // send the data to dashboard
+        dashboard.sendTelemetryPacket(packet);
+        // busy wait
+        Thread.sleep(10);
+    }
+
+    // written by rbrott, taken from drawing class in rr1.0 quickstart
+    public static void drawRobot(Canvas c, Pose2d t) {
+        final double ROBOT_RADIUS = 9;
+
+        c.setStrokeWidth(1);
+        c.strokeCircle(t.position.x, t.position.y, ROBOT_RADIUS);
+
+        Vector2d halfv = t.heading.vec().times(0.5 * ROBOT_RADIUS);
+        Vector2d p1 = t.position.plus(halfv);
+        Vector2d p2 = p1.plus(halfv);
+        c.strokeLine(p1.x, p1.y, p2.x, p2.y);
+    }
+    // also written by rbrott, taken from mecanumdrive class in rr1.0 quickstart
+    private void drawPoseList(Canvas c, ArrayList<Pose2d> poses) {
+        double[] xPoints = new double[poses.size()];
+        double[] yPoints = new double[poses.size()];
+
+        int i = 0;
+        for (Pose2d t : poses) {
+            xPoints[i] = t.position.x;
+            yPoints[i] = t.position.y;
+
+            i++;
+        }
+
+        c.strokePolyline(xPoints, yPoints);
+    }
+}
